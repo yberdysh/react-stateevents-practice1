2966c6804a3e2838b739035f4278e8940dd34219
Jane Costa <yberdysh@gmail.com>
2019-02-20 14:04:23 -0500

    revised solution

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 class DogCard extends React.Component {
+  state = {
+    clicked: false
+  }
+
+  clickBark = () => {
+    this.setState({clicked: !this.state.clicked})
+  }
   render() {
     return (
       <div>
-        <h2>{/*Dog name goes here*/}</h2>
-        <img alt="" src={""} />
-        <button onClick={""}>Bark</button>
+        <h2>{this.props.dog.name}</h2>
+        <div className="ruffContainer">
+          <img alt="" src={this.props.dog.img} />
+          {this.state.clicked && <h1>RUFF</h1>}
+        </div>
+        <button onClick={this.clickBark}>Bark</button>
       </div>
     );
   }
--- src/Containers/DogsList.js
+++ src/Containers/DogsList.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import DogCard from '../Components/DogCard'
+import dogs from '../dogs'
 
 class DogList extends Component {
+  state = {
+    dogs: dogs
+  }
   render() {
-    return <div className="dogContainer">{/*list of dogs go here*/}</div>;
+    return <div className="dogContainer">{this.state.dogs.map(dog => <DogCard key={dog.id} dog={dog}/>)}</div>;
   }
 }
 
