51e2f89a172369bfe4420e3d4aa63a8255743f18
Jane Costa <yberdysh@gmail.com>
2019-02-20 16:11:05 -0500

    completed review

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -1,12 +1,31 @@
 import React from "react";
 
 class DogCard extends React.Component {
+  state = {
+    clicked: false
+  }
+
+  // ruff(){
+
+  // }
+
+  ruff = () => {
+    this.setState({clicked: !this.state.clicked})
+  }
+
   render() {
+    console.log("PROPS", this.props)
+    console.log("STATE", this.state)
+    const {name, img} = this.props.dog
     return (
       <div>
-        <h2>{/*Dog name goes here*/}</h2>
-        <img alt="" src={""} />
-        <button onClick={""}>Bark</button>
+        <h2>{name}</h2>
+        <div className="dogRuff">
+          <img alt="" src={img} />
+          {this.state.clicked && <h1>RUFF</h1>}
+        </div>
+        <button onClick={this.ruff}>Bark</button>
+        {/*this.state.clicked ? <h1>RUFF</h1> : null*/}
       </div>
     );
   }
--- src/Containers/DogsList.js
+++ src/Containers/DogsList.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
+import dogs from '../dogs'
+import DogCard from '../Components/DogCard'
 
 class DogList extends Component {
+  // state = {
+  //   dogs: dogs
+  // }
+
+  constructor(props){
+    super(props)
+    this.state = {
+      dogs: dogs
+    }
+  }
+
+
+
   render() {
-    return <div className="dogContainer">{/*list of dogs go here*/}</div>;
+    let mappedDogs = this.state.dogs.map((dog, index) => <DogCard dog={dog} key={index}  />)
+    return <div className="dogContainer">{mappedDogs}</div>;
   }
 }
 
